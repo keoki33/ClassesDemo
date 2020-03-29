544179df2ff3b5d1a062ce21f978f89eae66a817
keoki33 <gwong323@yahoo.com>
2020-03-29 15:39:36 +1100

    finish class lesson

--- ClassesDemo/main.swift
+++ ClassesDemo/main.swift
@@ -1,12 +1,28 @@
-//
-//  main.swift
-//  ClassesDemo
-//
-//  Created by Keoki on 28/3/20.
-//  Copyright © 2020 Keoki. All rights reserved.
-//
 
-import Foundation
 
-print("Hello, World!")
+let skeleton = Enemy(health: 100, attackStrength: 10                )
+print(skeleton.health)
+skeleton.move()
+skeleton.attack()
 
+let skeleton2 = Enemy(health: 100, attackStrength: 10)
+let skeleton3 = Enemy(health: 90, attackStrength: 15)
+
+let skeleton5 = skeleton
+print(skeleton.health)
+print(skeleton5.health)
+skeleton.takeDamage(amount: 10)
+print(skeleton.health)
+print(skeleton5.health)
+
+
+let dragon = Dragon(health: 200, attackStrength: 30)
+
+dragon.move()
+dragon.wingSpan = 5
+dragon.attackStrength = 15
+
+dragon.attack()
+print(dragon.wingSpan)
+
+dragon.talk(speech: "Yo")
